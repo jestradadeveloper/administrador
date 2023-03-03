3f868e87c6412d470962576202ccb9666b3f293b
Jose Estrada <j.estrada.developer@gmail.com>
2023-03-03 11:54:00 -0600

    HOTFIX seed database

--- backend_api/db/seeds.rb
+++ backend_api/db/seeds.rb
@@ -8,13 +8,3 @@ include FactoryBot::Syntax::Methods
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #superadmin seed
-superadminuser = User.create(email:ENV['SUPERADMIN_EMAIL'], password: ENV['SUPERADMIN_PASSWORD'])
-if superadminuser.valid?
-  superadminuser.save
-end
-adminUser = User.create(email: 'admin2@admin.com', password: ENV['SUPERADMIN_PASSWORD'])
-if adminUser.valid?
-  adminUser.save
-end
-Team.create(name: Faker::Internet.username, description: Faker::Internet.username,  user_id: superadminuser.id)
-Account.create(name: 'Nuevas cuentas', client: Faker::Internet.username, user_id: superadminuser.id)
