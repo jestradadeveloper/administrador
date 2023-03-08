e913efcac55ef9711ab7e35b21ca8491598e26a4
Jose Estrada <j.estrada.developer@gmail.com>
2023-03-08 12:49:00 -0600

    HOTFIX  useEffect Account Page editMode false

--- frontend/src/pages/Accounts/Accounts.js
+++ frontend/src/pages/Accounts/Accounts.js
@@ -1,14 +1,17 @@
 import Accounts from "../../components/accounts/Accounts";
 import { DashboardLayout } from "../../components/layouts";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { getAccounts } from "../../store/accounts/thunks";
+import { getTeams } from "../../store/teams/thunks";
 import { setFormMode } from "../../store";
 
 export const AccountsPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setFormMode());
+    dispatch(getAccounts());
+    dispatch(getTeams());
   }, []);
   return (
     <DashboardLayout title="Mind Accounts">
