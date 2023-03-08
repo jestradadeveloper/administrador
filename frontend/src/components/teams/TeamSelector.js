ce6452489a9800798d091d996f73c1a2bc61cd80
Jose Estrada <j.estrada.developer@gmail.com>
2023-03-08 10:12:38 -0600

    HOTFIX active param

--- frontend/src/components/teams/TeamSelector.js
+++ frontend/src/components/teams/TeamSelector.js
@@ -1,23 +1,56 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+import { useSelector } from "react-redux";
+import { useController, useForm } from "react-hook-form";
+const TeamSelector = () => {
+  const { register, handleSubmit, formState, control } = useForm();
+  const { teams } = useSelector((state) => state.teams);
+  const { active } = useSelector((state) => state.accounts);
+  const [selectedId, setSelectedId] = useState("");
+  const [teamList, setTeamList] = useState([]);
 
-const TeamSelector = ({ teams, register }) => {
+  const { field } = useController({ name: "teamId", control });
+  const { value: teamValue, onChange: teamOnChange, ...restTeamField } = field;
+  const [defaultSelected, setDefaultSelected] = useState({
+    label: "",
+    value: "",
+  });
+
+  const handleSelectChange = (evt) => {
+    setSelectedId(evt);
+  };
+  useEffect(() => {
+    if (active) {
+      setDefaultSelected({ label: active["team-assigned"], value: active.id });
+    }
+  }, [active]);
+  useEffect(() => {
+    const tl = teams.map((team) => ({
+      value: team.id,
+      label: team.attributes.name,
+    }));
+    setTeamList(tl);
+  }, []);
+
   return (
     <>
       <label htmlFor="large" className="block my-2 font-bold text-red-500">
         Assign a team
       </label>
-      <select
-        id="team"
-        name="team"
-        className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-        {...register}
-      >
-        {teams.map((team) => (
-          <option value={`${team.id}`} key={team.id}>
-            {team.attributes.name}
-          </option>
-        ))}
-      </select>
+      {teamList && (
+        <Select
+          id="teamId"
+          options={teams.map((team) => ({
+            value: team.id,
+            label: team.attributes.name,
+          }))}
+          value={
+            teamValue ? teamList.find((x) => x.value === teamValue) : teamValue
+          }
+          onChange={(option) => teamOnChange(option ? option.value : option)}
+          {...restTeamField}
+        />
+      )}
     </>
   );
 };
